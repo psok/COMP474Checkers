7ea21542240bcb727513b0b5472edb8b5c0093f1
Richa Seth <rseth@luc.edu>
2015-02-24 14:38:38 -0600

    Added Comment

--- app/src/main/java/com/checkers/kingme/comp474checkers/Player.java
+++ app/src/main/java/com/checkers/kingme/comp474checkers/Player.java
@@ -7,35 +7,69 @@ public class Player {
     private String myName;
     private boolean isWinner;
 
+    /**
+     *
+     * @param myPieceColor
+     * @param name
+     */
     Player(Color myPieceColor, String name) {
         this.myPieceColor = myPieceColor;
         this.myName = name;
     }
 
+    /**
+     *
+     * @return
+     */
     public Color getMyPieceColor() {
         return myPieceColor;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMyName() {
         return myName;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isWinner() {
         return isWinner;
     }
 
+    /**
+     *
+     * @param myPieceColor
+     */
     public void setMyPieceColor(Color myPieceColor) {
         this.myPieceColor = myPieceColor;
     }
 
+    /**
+     *
+     * @param myName
+     */
     public void setMyName(String myName) {
         this.myName = myName;
     }
 
+    /**
+     *
+     * @param isWinner
+     */
     public void setWinner(boolean isWinner) {
         this.isWinner = isWinner;
     }
 
+    /**
+     *
+     * @param board
+     * @return
+     */
     public boolean getIsWinnerResult(CurrentBoard board) {
         this.isWinner = true;
         int opponentPiece = 0;
