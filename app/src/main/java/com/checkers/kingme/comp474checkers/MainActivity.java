411cfa2160ea002937b3189b13f4f05eee414774
alevelozo <alessandrac.velozo@gmail.com>
2015-02-26 00:54:29 -0600

    Updated the manifest.xml and strings.xml

--- app/src/main/java/com/checkers/kingme/comp474checkers/MainActivity.java
+++ app/src/main/java/com/checkers/kingme/comp474checkers/MainActivity.java
@@ -4,30 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends ActionBarActivity
     {
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         }
 
+        // Call game activity
         public void sendMessage(View view) {
             Intent intent = new Intent(this, Game.class);
             startActivity(intent);
         }
-
+        // Set the visibility of the start button
         public void startVisibility(View view) {
             ImageButton btnStart= (ImageButton) findViewById(R.id.startButton);
             btnStart.setVisibility(view.VISIBLE);
-
         }
 }
