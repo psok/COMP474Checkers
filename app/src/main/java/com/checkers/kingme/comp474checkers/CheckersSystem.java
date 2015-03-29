d1cc852685f0445ba009cd56e2959379a9e4ae7a
alevelozo <alessandrac.velozo@gmail.com>
2015-03-29 00:38:24 -0500

    Activities  player x player in the same device done

--- app/src/main/java/com/checkers/kingme/comp474checkers/CheckersSystem.java
+++ app/src/main/java/com/checkers/kingme/comp474checkers/CheckersSystem.java
@@ -2,7 +2,6 @@ package com.checkers.kingme.comp474checkers;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.GridLayout;
