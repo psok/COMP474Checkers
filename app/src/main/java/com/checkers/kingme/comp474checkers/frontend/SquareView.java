3a2c68a1e96ee240fbdca4ba91884c4941e17656
Richa Seth <rseth@luc.edu>
2015-04-30 12:22:58 -0500

    fixed the null pointer exception at end

--- app/src/main/java/com/checkers/kingme/comp474checkers/frontend/SquareView.java
+++ app/src/main/java/com/checkers/kingme/comp474checkers/frontend/SquareView.java
@@ -3,6 +3,7 @@ package com.checkers.kingme.comp474checkers.frontend;
 /**
  * Created by dalestoutjr on 2/20/15.
  */
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.view.View;
@@ -14,30 +15,31 @@ import com.checkers.kingme.comp474checkers.backend.Rank;
 public class SquareView extends View {
 
     public interface OnTapListener { // interface between view and system
+
         void onTap(int squareId);
     }
 
     private int squareID = 0; //default
     private Square square;
 
-    public SquareView(Context context, Square sqr, int squareId)
-    {
+    public SquareView(Context context, Square sqr, int squareId) {
         super(context);
         this.square = sqr;
         squareID = squareId;
     }
 
-    public void setOnTapListener (OnTapListener listener) {
+    public void setOnTapListener(OnTapListener listener) {
         final OnTapListener finalListener = listener;
         this.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                finalListener.onTap(squareID);
+                if (finalListener != null) {
+                    finalListener.onTap(squareID);
+                }
             }
         });
     }
 
-    public void updateSquare(Piece[] board)
-    {
+    public void updateSquare(Piece[] board) {
         if (squareID > 0) {
             square = square.removeDecorations();
 
@@ -59,13 +61,11 @@ public class SquareView extends View {
         }
     }
 
-    public void highlight()
-    {
+    public void highlight() {
         square = new Highlight(square);
     }
 
-    public void unHighlight()
-    {
+    public void unHighlight() {
         square = square.removeDecoration();
     }
 
